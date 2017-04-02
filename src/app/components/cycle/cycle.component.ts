6327f2faa4d2b5854e868c8b4b6ceea6d93b78a6
Carlos Rodríguez <carlos@beatbcn.com>
2017-04-02 13:47:02 +0100

    Display

--- src/app/components/cycle/cycle.component.ts
+++ src/app/components/cycle/cycle.component.ts
@@ -12,28 +12,35 @@ let io = require('socket.io-client');
 export class CycleComponent {
     private name:string;
     private socket:any
+    private i:number;
+    private last_value:number;
 
 
     constructor(){
-        this.name = "Cycle"
+        this.i=0;
+        this.name = "Cycle Trading"
         this.socket = io('https://cryptodeskbackend.herokuapp.com/');
-        this.start();
+        setTimeout(this.start(), 2000);
         this.socket.on('info', function(data:any){
             console.log(data);
-            if(data.finished){
-                alert("Your revenue has been: "+(this.changes[this.changes.length-1]-0.4)*100/0.4 + "%");
-            }
         });
         this.socket.on('movement', function(data:any){
+            let jdata=JSON.parse(data);
             //this.changes.push(new Change(data.to, data.new_amount));
+            $('#step_container').append('<div style="width: 100px;" class="pearl done col-xs-4"> <span class="pearl-number"></span> <span class="pearl-title">'+jdata.to+'</span> </div>');
+            $('#description').append('<h4>Exchanged '+jdata.actual_amount+' '+jdata.from+' for '+jdata.new_amount+' '+jdata.to+'.</h4>');
+            this.i=this.i+1;
+            this.last_value=jdata.new_amount;
             console.log(data);
+            if(jdata.to == "BTC"){
+                $('#description').append("<h3>Your revenue has been: "+(this.last_value-0.4)*100/0.4 + "%</h3>");
+            }
         });
     }
 
     start(){
         //this.changes.push(new Change("BTC", 0.4));
         this.socket.emit('start', {});
-        alert("start");
     }
 
 
