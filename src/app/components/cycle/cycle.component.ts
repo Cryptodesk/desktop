2bc1f519cfc945b464642faf9b9947c35bf7b311
Carlos Rodríguez <carlos@beatbcn.com>
2017-04-02 12:34:47 +0100

    Final Changes

--- src/app/components/cycle/cycle.component.ts
+++ src/app/components/cycle/cycle.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Change} from "../../objects/change";
+
+let io = require('socket.io-client');
 
 @Component({
     selector: 'app-cycle',
@@ -8,8 +11,30 @@ import { Component } from '@angular/core';
 
 export class CycleComponent {
     private name:string;
+    private socket:any
+
 
     constructor(){
         this.name = "Cycle"
+        this.socket = io('https://cryptodeskbackend.herokuapp.com/');
+        this.start();
+        this.socket.on('info', function(data:any){
+            console.log(data);
+            if(data.finished){
+                alert("Your revenue has been: "+(this.changes[this.changes.length-1]-0.4)*100/0.4 + "%");
+            }
+        });
+        this.socket.on('movement', function(data:any){
+            //this.changes.push(new Change(data.to, data.new_amount));
+            console.log(data);
+        });
     }
+
+    start(){
+        //this.changes.push(new Change("BTC", 0.4));
+        this.socket.emit('start', {});
+        alert("start");
+    }
+
+
 }
---
+++ src/app/objects/change.ts
@@ -0,0 +1,9 @@
+export class Change {
+    public currency:string;
+    public money:number;
+
+    constructor(currency:string, money:number){
+        this.currency=currency;
+        this.money=money;
+    }
+}
